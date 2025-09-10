eafd3004d38a83f3d41c271aa5a1548d9f5f906d
Yohanes Christian <yohanesdevano90@gmail.com>
2025-09-10 12:52:55 +0700

    function variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello_test() {
+    println!("Hello test")
+}
+
+#[test]
+fn test_variable() {
+    let mut name= "joko";
+    println!("Hello {}", name);
+
+    name = "marni";
+    println!("Hello {}", name)
+}
+#[test]
+fn static_typing() {
+    let name= "joko";
+    println!("Hello {}", name);
+
+    // name = 20;
+    println!("Hello {}", name)
+}
+
+#[test]
+fn shadowing() {
+    let name= "joko";
+    println!("Hello {}", name);
+
+    let name = "budiono";
+    println!("Hello {}", name)
+}
+
