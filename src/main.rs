f1ad49a4ea545cead2a7625ab35fd955ad7d4d7f
Yohanes Christian <yohanesdevano90@gmail.com>
2025-09-10 20:12:34 +0700

    update variabel operation

--- src/main.rs
+++ src/main.rs
@@ -33,3 +33,58 @@ fn shadowing() {
     println!("Hello {}", name)
 }
 
+// terdapat beberapa tipe data untuk integer
+// 8, 16, 32, 64, 128
+// ukuran default bilangan bulat i32 dan desimal f64
+#[test]
+fn explicit() {
+    let age: i32= 15;
+    println!("Saya berumur {}", age);
+}
+
+#[test]
+fn number() {
+    let a: i32 = 10;
+    println!("Angka {}", a);
+
+    let b: f32 = 10.6;
+    println!("Angka {}", b); 
+}
+
+#[test]
+fn number_conversion() {
+    let a: i8 = 10;
+    println!("Angka {}", a);
+
+    let b: i16 = a as i16;
+    println!("Angka {}", b); 
+
+    let b: i32 = b as i32;
+    println!("Angka {}", b); 
+
+
+    let overflow: i64 = 1000000000;
+    let temp_overflow: i8 = overflow as i8;
+
+    println!("{}", temp_overflow) 
+}
+
+#[test]
+fn number_operation() {
+    let angka1 = 10;
+    let angka2 = 12;
+    let hasil = angka1*angka2;
+
+    print!("{}", hasil)
+}
+
+#[test]
+fn augmented_assingment() {
+    let mut angka1 = 10;
+    let mut angka2 = 12;
+    angka1+=30;
+    // angka1-=30;
+    // angka1*=30;
+
+    print!("{}", angka1)
+}
